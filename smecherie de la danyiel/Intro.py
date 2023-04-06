1b863870e1066b523ccf82091c61e972790f5774
Dyva2 <130086424+Dyva2@users.noreply.github.com>
2023-04-06 20:16:46 +0300

    Add files via upload

---
+++ smecherie de la danyiel/Intro.py
@@ -0,0 +1,26 @@
+numar = 1.2
+nume = "gigel"
+
+print(type(numar))
+print("numar ales:   ")
+print(numar)
+print(type(nume))
+
+
+if 0<1:
+    print("adeverat")
+else: print("fals")
+
+i=1
+while i<10:
+     print(i)
+     i=i+1
+
+j=1
+
+
+
+
+
+
+
---
+++ smecherie de la danyiel/test.py
@@ -0,0 +1,30 @@
+
+numele = input ("Care este numle tau?")
+print ("Salut,", numele)
+dn = input ("vrei sa incepi")
+if dn== "da":
+    print ("hai sa incepem")
+elif dn== "nu":   
+    print ("......")
+print ("Este cu adevarat numele tau", numele,"?")
+
+dn1 = input ("da sau nu?")
+if dn1=="da":
+    print("CORECT")
+elif dn1== "nu":
+    print ("GRESIT")
+print ("Este cu adevarat 1+1 = 2?")
+dn2 = input ("da sau nu?")
+if dn2=="da":
+    print("CORECT")
+elif dn2== "nu":
+    print ("GRESIT")
+dn3 = input ("Care este capitala romaniei?")
+if dn3=="Bucuresti":
+    print("CORECT")
+else:
+    print ("GRESIT")
+
+
+
+
